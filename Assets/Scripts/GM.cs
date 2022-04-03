fe8fe0b6f8c8f72b4319d9a5b25bb170d961b6fd
GeoEngel <GeoEngel.z@gmail.com>
2022-04-03 03:09:34 -0400

    Updated GM Strike Event to use delegate

--- Assets/Scripts/GM.cs
+++ Assets/Scripts/GM.cs
@@ -23,51 +23,22 @@ public class GM : MonoBehaviour
     }
 
     [SerializeField] private int strikes = 0;
-    public event EventHandler ResetEvent;
-    public event EventHandler FirstStrikeEvent;
-    public event EventHandler SecondStrikeEvent;
-    public event EventHandler ThirdStrikeEvent; // Effectively ends the game loop!
+    public delegate void StrikeEventHandler(int strikeCount);
+    public event StrikeEventHandler StrikeEvent;
+    // public event EventHandler ResetEvent;
 
     // I want to trigger various behavioral changes based on game "state"...
     // So this should send out a "Message" right? As in a messaging system?
     // Essentially an "Event" to update the necessary processes to change behavior state!
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void Strike(){
         strikes++;
-        switch (strikes)
-        {
-            case 1:
-                FirstStrikeEvent?.Invoke(this, EventArgs.Empty);
-                break;
-            case 2:
-                SecondStrikeEvent?.Invoke(this, EventArgs.Empty);
-                break;
-            case 3:
-                ThirdStrikeEvent?.Invoke(this, EventArgs.Empty);
-                break;
-            default:
-                Debug.LogError("Invalid Strike Value!");
-                break;
-        }
-        // Should send a message update whenever "Strikes" value is changed!
+        StrikeEvent?.Invoke(strikes);
     }
 
     void Reset(){
         strikes = 0;
-        ResetEvent?.Invoke(this, EventArgs.Empty);
-        // Should send a message update whenever "Strikes" value is changed!
+        StrikeEvent?.Invoke(strikes);
+        // ResetEvent?.Invoke(this, EventArgs.Empty);
     }
 }
