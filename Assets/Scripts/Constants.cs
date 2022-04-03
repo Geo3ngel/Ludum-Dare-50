7674dbdbccf421e756a7813523a39d8c681d2279
GeoEngel <GeoEngel.z@gmail.com>
2022-04-03 03:09:13 -0400

    Added strike values to constants

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -2,4 +2,9 @@ static class Constants{
     // Game over cause values
     public const int TIME_OVERFLOW = 0;
     public const int OUT_OF_TIME = 1;
+
+    public const int NO_STRIKES = 0;
+    public const int FIRST_STRIKE = 1;
+    public const int SECOND_STRIKE = 2;
+    public const int THIRD_STRIKE = 3;
 }
